ba1de49e32a7952c1f9b5f903789c9fd08e8e6d6
gbrllcavichion <cavichionmichel@gmail.com>
2024-10-19 21:48:16 -0300

    :sparkles: Page show

--- app/src/main/java/com/example/showfinderui/ShowDetailsActivity.kt
+++ app/src/main/java/com/example/showfinderui/ShowDetailsActivity.kt
@@ -12,14 +12,11 @@ class ShowDetailsActivity : AppCompatActivity() {
 
         val artistName = intent.getStringExtra("artist_name")
         val tourName = intent.getStringExtra("tour_name")
-        val location = intent.getStringExtra("location")
 
         val tvArtistName: TextView = findViewById(R.id.tv_artist_name)
         val tvTourName: TextView = findViewById(R.id.tv_tour_name)
-        val tvLocation: TextView = findViewById(R.id.tv_show_location)
 
         tvArtistName.text = artistName
         tvTourName.text = tourName
-        tvLocation.text = location
     }
 }
